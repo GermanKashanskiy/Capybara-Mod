095ba3d1c7295b66edcc3580f7735efe56cce11d
GermanKashanskiy <gitipim@yandex.com>
2023-04-25 15:51:27 +0300

    Added code for Mystical Mushroom (Food Item)

--- src/main/java/dev/gitipi/capybara/client/models/CapybaraModel.java
+++ src/main/java/dev/gitipi/capybara/client/models/CapybaraModel.java
@@ -1,7 +1,4 @@
-package dev.gitipi.capybara.client.models;// Made with Blockbench 4.7.0
-// Exported for Minecraft version 1.17 or later with Mojang mappings
-// Paste this class into your mod and generate all required imports
-
+package dev.gitipi.capybara.client.models;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
--- src/main/java/dev/gitipi/capybara/init/ItemInit.java
+++ src/main/java/dev/gitipi/capybara/init/ItemInit.java
@@ -1,6 +1,7 @@
 package dev.gitipi.capybara.init;
 
 import dev.gitipi.capybara.Capybara;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeSpawnEggItem;
@@ -11,15 +12,18 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Capybara.MODID);
 
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
-            () -> new Item(props()));
+    public static final RegistryObject<Item> MYSTIC_MUSHROOM = ITEMS.register("mystic_mushroom",
+            () -> new Item(propsFood().food(new FoodProperties.Builder().nutrition(20).fast().build())));
 
 
     public static final RegistryObject<ForgeSpawnEggItem> EXAMPLE_ENTITY_SPAWN_EGG = ITEMS.register("capybara_entity_spawn_egg",
-            () -> new ForgeSpawnEggItem(EntityInit.CAPYBARA, 0xFF55AA, 0x27DA9F, props().stacksTo(16)));
+            () -> new ForgeSpawnEggItem(EntityInit.CAPYBARA, 0xFF55AA, 0x27DA9F, propsMisc().stacksTo(16)));
 
-    private static Item.Properties props() {
+    private static Item.Properties propsMisc() {
         return new Item.Properties().tab(CreativeModeTab.TAB_MISC);
+    }
+    private static Item.Properties propsFood() {
+        return new Item.Properties().tab(CreativeModeTab.TAB_FOOD);
     }
 
 }
