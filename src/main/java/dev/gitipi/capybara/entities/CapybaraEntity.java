93a1a80fb4527d71d2d5019ccfb1eb7d127d66cd
GermanKashanskiy <gitipim@yandex.com>
2023-04-25 17:09:59 +0300

    Added logic for CapybaraEntity (CapyGoToWaterGoal)

--- src/main/java/dev/gitipi/capybara/entities/CapybaraEntity.java
+++ src/main/java/dev/gitipi/capybara/entities/CapybaraEntity.java
@@ -11,14 +11,16 @@ import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Turtle;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.Nullable;
 
-public class CapybaraEntity extends Animal {
-    public CapybaraEntity(EntityType<? extends Animal> type, Level level) {
+public class CapybaraEntity extends Turtle {
+    public CapybaraEntity(EntityType<? extends Turtle> type, Level level) {
         super(type, level);
     }
 
@@ -30,7 +32,8 @@ public class CapybaraEntity extends Animal {
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(0, new CapyGoToWaterGoal(this, 1.0D));
+        this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
         this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
@@ -40,10 +43,33 @@ public class CapybaraEntity extends Animal {
     }
 
     public static AttributeSupplier.Builder getExampleAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 25.0D).add(Attributes.MOVEMENT_SPEED, 0.15D);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 25.0D).add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 
     public static boolean canSpawn(EntityType<CapybaraEntity> entityType, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
         return checkAnimalSpawnRules(entityType, level, spawnType, pos, random) && pos.getY() > 50;
+    }
+
+    static class CapyGoToWaterGoal extends MoveToBlockGoal {
+        private static final int GIVE_UP_TICKS = 1200;
+        private final Turtle capybara;
+
+        CapyGoToWaterGoal(Turtle turtle, double p_30263_) {
+            super(turtle, turtle.isBaby() ? 2.0D : p_30263_, 24);
+            this.capybara = turtle;
+            this.verticalSearchStart = -1;
+        }
+
+        public boolean canContinueToUse() {
+            return !this.capybara.isInWater() && this.tryTicks <= 1200 && this.isValidTarget(this.capybara.level, this.blockPos);
+        }
+
+        public boolean shouldRecalculatePath() {
+            return this.tryTicks % 160 == 0;
+        }
+
+        protected boolean isValidTarget(LevelReader p_30270_, BlockPos p_30271_) {
+            return p_30270_.getBlockState(p_30271_).is(Blocks.WATER);
+        }
     }
 }
